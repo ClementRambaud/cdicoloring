6ce8136b6b19902ea0bff759ec7853b96e903fcc
Clement Rambaud <clement.rambaud@ens.psl.eu>
2020-11-01 14:32:19 +0100

    modifié :         find_3critical.sh

--- find_3critical.sh
+++ find_3critical.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-pick="pickg -d2: -u2: -C"
-main="./is_kcritical.native 3"
+geng -C -d4 $@ | parallel --block-size 140 --pipe "directg -o | ./is_kcritical.native 3"
 
-geng -C -d4 $@ | \
-  tee  >(awk 'NR % 4 == 0' | directg -o | $main) \
-       >(awk 'NR % 4 == 1' | directg -o | $main) \
-       >(awk 'NR % 4 == 2' | directg -o | $main) \
-       >(awk 'NR % 4 == 3' | directg -o | $main) \
-       > /dev/null
