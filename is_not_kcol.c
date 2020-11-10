26f02d17cb2f6ed9508640f6b487cb76f85f4997
Clement Rambaud <clement.rambaud@ens.psl.eu>
2020-11-10 16:25:45 +0100

    modifié :         is_not_kcol.c

--- is_not_kcol.c
+++ is_not_kcol.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
       read_digraph6(stdin, d, &n);
       if (feof(stdin)) break;
       // print_graph(stderr, d, n);
-      if ((deg_out_min(d,n) >= k) && (deg_in_min(d,n) >= 3) &&!is_kcol(d, n, k))
+      if (!is_kcol(d, n, k))
       {
         write_digraph6(stdout, d, n);
       }
