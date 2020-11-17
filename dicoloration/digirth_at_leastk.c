a35ce89b8f74ca86793f771893dd1c4e57b3b672
Clement Rambaud <clement.rambaud@ens.psl.eu>
2020-11-17 13:14:36 +0100

    modifié :         Makefile 	nouveau fichier : dicoloration/digirth_at_leastk.c

---
+++ dicoloration/digirth_at_leastk.c
@@ -0,0 +1,19 @@
+#include "dicoloration.h"
+
+int main(int argc, char *argv[])
+{
+    char k = argv[1][0] - 48;
+    graph d[MAXN];
+    int n;
+    while (1)
+    {
+      read_digraph6(stdin, d, &n);
+      if (feof(stdin)) break;
+      // print_graph(stderr, d, n);
+      if (digirth_at_leastk(d, n, k))
+      {
+        write_digraph6(stdout, d, n);
+      }
+    }
+}
+
